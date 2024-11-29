434ed86e2da56220241f3a278d72673f299f6b71
miomua <33140872+miomua@users.noreply.github.com>
2024-11-29 18:38:48 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -74,8 +74,9 @@ lazy_static::lazy_static! {
         let mut map = HashMap::new();
         map.insert("password".to_string(), "xdj123!@#".to_string());
         RwLock::new(map)
-    };
-}
+    }; 
+    pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+} 
 lazy_static::lazy_static! {
     pub static ref APP_DIR: RwLock<String> = Default::default();
 }
